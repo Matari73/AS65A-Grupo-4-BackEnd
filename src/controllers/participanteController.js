b0ff31d47111b4b30d8302b1cb6478dcddf4f76e
Matari73 <marianadeoliveira73@hotmail.com>
2024-11-20 15:31:17 -0300

    refatoração participanteController

--- src/controllers/participanteController.js
+++ src/controllers/participanteController.js
@@ -1,10 +1,10 @@
 const Participante = require("../models/Participante.js");
-
 class ParticipanteController {
     static async criarParticipante(req, res) {
         const { anonimo, nome, endereco, contato } = req.body;
+
         try {
-            if (!anonimo) {
+            if (!anonimo && nome) {
                 const participanteExiste = await Participante.findOne({ where: { nome } });
                 if (participanteExiste) {
                     console.log("Participante já existe");
@@ -14,9 +14,9 @@ class ParticipanteController {
 
             const novoParticipante = await Participante.create({
                 anonimo,
-                nome,
-                endereco,
-                contato
+                nome: anonimo ? null : nome,
+                endereco: anonimo ? null : endereco,
+                contato: anonimo ? null : contato
             });
 
             console.log("Participante criado:", novoParticipante);
@@ -30,25 +30,24 @@ class ParticipanteController {
     static async listarParticipantes(req, res) {
         try {
             const participantes = await Participante.findAll();
-            console.log(participantes)
             res.status(200).json(participantes);
         } catch (error) {
             console.error('Erro ao buscar participantes:', error);
             res.status(500).json({ message: 'Erro ao buscar participantes.' });
         }
-    };
+    }
 
     static async listarParticipantePorNome(req, res) {
         const { nome } = req.params;
 
         try {
-            const participante = await Participante.findAll({ where: { nome } })
+            const participante = await Participante.findOne({ where: { nome } });
             if (!participante) {
-                return res.status(404).json({ message: 'participante não encontrado.' });
+                return res.status(404).json({ message: 'Participante não encontrado.' });
             }
             res.status(200).json(participante);
         } catch (error) {
-            console.error('Erro ao buscar o participante:', error);
+            console.error(`Erro ao buscar o participante com nome '${nome}':`, error);
             res.status(500).json({ message: 'Erro ao buscar o participante.' });
         }
     }
@@ -56,40 +55,32 @@ class ParticipanteController {
     static async atualizarParticipantePorNome(req, res) {
         const { nome } = req.params;
         const { anonimo, endereco, contato } = req.body;
+
         try {
-            const participante = await Participante.findOne(nome);
+            const participante = await Participante.findOne({ where: { nome } });
             if (!participante) {
-                return res.status(404).json({ message: 'participante não encontrado.' });
+                return res.status(404).json({ message: 'Participante não encontrado.' });
             }
 
-            if (anonimo) {
-                await participante.update({
-                    anonimo,
-                    nome: null,
-                    endereco: null,
-                    contato: null
-                });
+            await participante.update({
+                anonimo,
+                nome: anonimo ? null : participante.nome,
+                endereco: anonimo ? null : endereco,
+                contato: anonimo ? null : contato
+            });
 
-            }
-            else {
-                await participante.update({
-                    anonimo,
-                    endereco,
-                    contato
-                });
-            }
-
-            res.status(200).json({ message: 'participante atualizado com sucesso.', participante });
+            res.status(200).json({ message: 'Participante atualizado com sucesso.', participante });
         } catch (error) {
-            console.error('Erro ao atualizar o participante:', error);
+            console.error(`Erro ao atualizar o participante com nome '${nome}':`, error);
             res.status(500).json({ message: 'Erro ao atualizar o participante.' });
         }
     }
 
     static async deletarParticipantePorNome(req, res) {
         const { nome } = req.params;
+
         try {
-            const participante = await Participante.findOne(nome);
+            const participante = await Participante.findOne({ where: { nome } });
             if (!participante) {
                 return res.status(404).json({ message: 'Participante não encontrado.' });
             }
@@ -97,7 +88,7 @@ class ParticipanteController {
             await participante.destroy();
             res.status(200).json({ message: 'Participante deletado com sucesso.' });
         } catch (error) {
-            console.error('Erro ao deletar o participante:', error);
+            console.error(`Erro ao deletar o participante com nome '${nome}':`, error);
             res.status(500).json({ message: 'Erro ao deletar o participante.' });
         }
     }
